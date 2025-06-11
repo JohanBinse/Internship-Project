152b81056a47adab986da7935b16bd4f6ffdf7fe
hari kuttan <harikuttan@gmail.com>
2025-06-11 14:19:36 +0530

    Value

---
+++ server.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const cors=require("cors");
+const app=express();
+const port = 3001;
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/getdata/:id',(req,res)=>{
+    console.log(req.params);
+    res.send("Hello World 3");
+});
+app.post('/createdata', (req,res) => {
+    const data = req.body;
+    console.log(data);
+    res.send('Data received');
+})
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log("server running",{port});
+});
